1d89a15758a45d33f48e7cdbf4001c2b02606a72
james gill <jimmygill28@hotmail.com>
2021-01-24 17:38:34 +0000

    login and register validation

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,99 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
+
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../utlis/validators");
+const { SECRET_KEY } = require("../../config");
+const User = require("../../models/User");
+
+function generalToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+}
+
+module.exports = {
+  Mutation: {
+    async login(_, { username, password }) {
+      const { valid, errors } = validateLoginInput(username, password);
+      const user = await User.findOne({ username });
+
+      if (!valid) {
+        throw new UserInputError("Error", { errors });
+      }
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong credentials";
+        throw new UserInputError("Wrong credentials", { errors });
+      }
+
+      const token = generalToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+    async register(
+      _,
+      { registerInput: { username, email, password, comfirmPassword } }
+    ) {
+      //validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        comfirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      //make sure user doesnt already exist
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
+      }
+
+      //hash the password and create auth token
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      const res = await newUser.save();
+
+      const token = generalToken(res);
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
+    },
+  },
+};
