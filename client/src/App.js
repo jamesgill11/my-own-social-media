44ee12c151324b42f19c8e9fb5cc8a0b19d61ae3
james gill <jimmygill28@hotmail.com>
2021-01-26 16:57:00 +0000

    started frontend, apolloclient and semantic ui

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "semantic-ui-css/semantic.min.css";
+
+import "./App.css";
+
+import MenuBar from "./Components/MenuBar";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+
+function App() {
+  return (
+    <Router>
+      <div className="ui container">
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
