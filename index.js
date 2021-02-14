96c26745b9dc14ef392e3fc2343b17b5dd328bdf
james gill <jimmygill28@hotmail.com>
2021-02-14 11:38:44 +0000

    fixed env for server

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const { MONGODB } = require("./config");
 
 const pubsub = new PubSub();
 
+const PORT = process.env.port || 5000;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -18,8 +20,11 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log("MongoDB Connected!!");
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server is running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
   });
